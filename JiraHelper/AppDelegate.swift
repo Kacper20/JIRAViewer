750301bd004e0e9fa5c139005bcbac56fc68008f
Kacper Harasim <kacper.harasim@polidea.com>
2017-01-25 22:42:41 +0100

    Initial Commit

---
+++ JiraHelper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  JiraHelper
+//
+//  Created by Kacper Harasim on 25.01.2017.
+//  Copyright © 2017 Kacper Harasim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
