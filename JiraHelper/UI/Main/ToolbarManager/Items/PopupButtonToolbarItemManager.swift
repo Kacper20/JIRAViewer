ee7c8d73cffa80ddc09212eb96da2e179f476fcd
Kacper Harasim <kacper.harasim@polidea.com>
2017-06-17 17:48:30 +0200

    Created first version of touch bar(that does nothing at the moment

--- JiraHelper/UI/Main/ToolbarManager/Items/PopupButtonToolbarItemManager.swift
+++ JiraHelper/UI/Main/ToolbarManager/Items/PopupButtonToolbarItemManager.swift
@@ -5,20 +5,29 @@
 //  Created by Kacper Harasim on 16/06/2017.
 //  Copyright © 2017 Kacper Harasim. All rights reserved.
 //
-
 import Foundation
+import RxSwift
 
 final class PopupButtonToolbarItemManager {
 
     var item: NSToolbarItem!
     let identifier: String
+    private let button: NSPopUpButton
+    private let buttonSelectionSubject = PublishSubject<Int>()
+
+    var selectionChanges: Observable<Int> {
+        return buttonSelectionSubject
+    }
 
     init(identifier: String, texts: [String], selectedIndex: Int) {
         self.identifier = identifier
         let button = NSPopUpButton()
+        self.button = button
         button.addItems(withTitles: texts)
         button.selectItem(at: selectedIndex)
         button.sizeToFit()
+        button.target = self
+        button.action = #selector(PopupButtonToolbarItemManager.buttonOptionChanged)
 
         item = ToolbarItemProvider.customToolbarItem(
             itemForItemIdentifier: identifier,
@@ -30,6 +39,10 @@ final class PopupButtonToolbarItemManager {
             action: #selector(PopupButtonToolbarItemManager.targetAction),
             menu: nil
         ) ?? NSToolbarItem()
+    }
+
+    @objc private func buttonOptionChanged() {
+        buttonSelectionSubject.onNext(button.indexOfSelectedItem)
     }
 
     @objc private func targetAction() { }
