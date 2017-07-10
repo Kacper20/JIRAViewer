bbc3ba8cbce1948840dfe5704856d500d57f57e0
Kacper Harasim <kacper.harasim@polidea.com>
2017-07-10 20:14:13 +0200

    Created request for performing transition

--- JiraHelper/Model/JIRA/Sprint/Issues/Transitions/IssueTransitionsEndpoints.swift
+++ JiraHelper/Model/JIRA/Sprint/Issues/Transitions/IssueTransitionsEndpoints.swift
@@ -17,11 +17,45 @@ enum IssueTransitionsEndpoints {
     static func getTransitions(for issue: IssueIdentifiable) -> EndpointConfiguration<GetTransitionsResult> {
         return EndpointConfiguration(
             path: "/api/latest/issue/\(issue.id)/transitions",
-            method: .put,
+            method: .get,
             encoding: JSONEncoding.default,
             headers: [:],
             parameters: [:],
             resourceType: .json
+        )
+    }
+
+    struct TransitionCommand: Encodable {
+        let transition: IssueTransition
+
+        private enum CodingKeys: String, CodingKey {
+            case transition
+        }
+
+        private enum TransitionCodingKeys: String, CodingKey {
+            case id
+        }
+
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            var transitionInfo = container.nestedContainer(keyedBy: TransitionCodingKeys.self, forKey: .transition)
+            try transitionInfo.encode(transition.id, forKey: .id)
+        }
+    }
+
+    static func performTransition(
+        _ transition: IssueTransition,
+        for issue: IssueIdentifiable
+        ) -> EndpointConfiguration<Nothing> {
+        let command = TransitionCommand(transition: transition)
+        let encoder = JSONEncoder()
+        return EndpointConfiguration(
+            path: "/api/latest/issue/\(issue.id)/transitions",
+            method: .post,
+            encoding: JSONEncoding.default,
+            headers: [:],
+            parameters: [:],
+            resourceType: .none(Nothing())
         )
     }
 }
